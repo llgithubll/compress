adf0923cede86533ab30bd6716afa1f148b1e503
llgithubll <1468645630@qq.com>
2019-03-27 14:24:27 +0800

    Huffman压缩算法, evaluate重构

---
+++ Huffman.py
@@ -0,0 +1,165 @@
+import heapq
+from bitio import BitReader, BitWriter
+
+
+class Huffman:
+    """
+    Huffman压缩, 对字符(字节)统计频率, 并以此重新编码, 实现编码比特位最少
+    """
+    char_bit_len = 8  # 字符字节位数
+    num_bit_len = 32  # 数字字节位数(用来记录文本长度)
+
+    class Node:
+        """Huffman树的节点"""
+        def __init__(self, ch, freq, left=None, right=None):
+            self.ch = ch
+            self.freq = freq
+            self.left = left
+            self.right = right
+
+        def __lt__(self, other):
+            return self.freq < other.freq
+
+        def __eq__(self, other):
+            return self.freq == other.freq
+
+        def is_leaf(self):
+            return self.left is None and self.right is None
+
+    @staticmethod
+    def _build_trie(freq):
+        """
+        构建Huffman单词查找树
+        :param freq: 字母频率
+        :return:
+        """
+        min_heap = []
+        for ch, f in freq.items():
+            heapq.heappush(min_heap, Huffman.Node(ch, f))
+
+        while len(min_heap) > 1:
+            left = heapq.heappop(min_heap)
+            right = heapq.heappop(min_heap)
+            parent = Huffman.Node(None, left.freq + right.freq, left, right)
+            heapq.heappush(min_heap, parent)
+        return heapq.heappop(min_heap)
+
+    @staticmethod
+    def _write_trie(node, writer):
+        """
+        将trie写入压缩文件, 解压时用
+        :param node: 根节点
+        :param writer: 写入组件
+        :return:
+        """
+        if node.is_leaf():
+            writer.write_bit(True)
+            writer.write_bits(node.ch, Huffman.char_bit_len)
+        else:
+            writer.write_bit(False)
+            Huffman._write_trie(node.left, writer)
+            Huffman._write_trie(node.right, writer)
+
+    @staticmethod
+    def _build_code(code_table, node, s):
+        """
+        构建Huffman code映射表, lookup_table
+        :param code_table: 编码表
+        :param node: 根节点
+        :param s: 字符串
+        :return:
+        """
+        if node.is_leaf():
+            code_table[node.ch] = s
+        else:
+            Huffman._build_code(code_table, node.left, s + '0')
+            Huffman._build_code(code_table, node.right, s + '1')
+
+    @staticmethod
+    def _read_trie(reader):
+        is_leaf = reader.read_bit()
+        if is_leaf:
+            return Huffman.Node(reader.read_bits(Huffman.char_bit_len), 0, None, None)
+        else:
+            return Huffman.Node(None, 0, Huffman._read_trie(reader), Huffman._read_trie(reader))
+
+    @staticmethod
+    def compress(origin_filepath, compress_filepath):
+        """
+        将``原始文件``压缩到``压缩文件``中
+        :param origin_filepath: 原始文件
+        :param compress_filepath: 压缩文件
+        :return: 没有返回值
+        """
+        # 统计频率(一轮读取)
+        freq = {}
+        text_len = 0
+        with open(origin_filepath, 'rb') as ori_f:
+            with BitReader(ori_f) as reader:
+                while True:
+                    ch = reader.read_bits(Huffman.char_bit_len)
+                    if not reader.read:
+                        break
+                    freq[ch] = freq.get(ch, 0) + 1
+                    text_len += 1
+
+        ori_f = open(origin_filepath, 'rb')
+        com_f = open(compress_filepath, 'wb')
+
+        with BitReader(ori_f) as reader:
+            with BitWriter(com_f) as writer:
+                root = Huffman._build_trie(freq)  # 构建Huffman树
+                code_table = {}
+                Huffman._build_code(code_table, root, '')  # 构建Huffman编码映射表
+                Huffman._write_trie(root, writer)  # 将trie写入压缩文件, 解压时用
+                writer.write_bits(text_len, Huffman.num_bit_len)  # 写入输入长度
+
+                # 使用Huffman code编码文件(二轮读取)
+                while True:
+                    ch = reader.read_bits(Huffman.char_bit_len)
+                    if not reader.read:
+                        break
+                    code = code_table[ch]
+                    for b in code:
+                        if b == '0':
+                            writer.write_bit(False)
+                        elif b == '1':
+                            writer.write_bit(True)
+                        else:
+                            raise Exception('Illegal state')
+        ori_f.close()
+        com_f.close()
+
+    @staticmethod
+    def expand(compress_filepath, origin_filepath):
+        """
+        将``压缩文件``, 解压写到``原始文件``中
+        :param compress_filepath: 压缩文件
+        :param origin_filepath: 原始文件
+        :return: 不返回任何值
+        """
+        com_f = open(compress_filepath, 'rb')
+        ori_f = open(origin_filepath, 'wb')
+
+        with BitReader(com_f) as reader:
+            with BitWriter(ori_f) as writer:
+                root = Huffman._read_trie(reader)
+                text_len = reader.read_bits(Huffman.num_bit_len)
+                for i in range(text_len):
+                    x = root
+                    while not x.is_leaf():
+                        if reader.read_bit():
+                            x = x.right
+                        else:
+                            x = x.left
+                    writer.write_bits(x.ch, Huffman.char_bit_len)
+        com_f.close()
+        ori_f.close()
+
+
+if __name__ == '__main__':
+    src_fp = 'data/tinytinyTale.txt'
+    com_fp = 'temp_files/tinytinyTale.txt.huffman'
+    exp_fp = 'temp_files/tinytinyTale.txt'
+    Huffman.compress(src_fp, com_fp)
+    Huffman.expand(com_fp, exp_fp)
--- evaluate.py
+++ evaluate.py
@@ -1,25 +1,57 @@
 """
 检测压缩算法的准确性, 评估压缩率, 效率
 """
-from bitstring import BitStream
+import time
+
+from bitio import BitReader
 from RunLength import RunLength
+from Huffman import Huffman
 
 
 class FileBits:
     def __init__(self, fp):
-        self.bits = BitStream(open(fp, 'rb').read())
+        self.f = open(fp, 'rb')
+        self.bit_stream = BitReader(self.f)
+        self.bits = []
+        while True:
+            x = self.bit_stream.read_bit()
+            if not self.bit_stream.read:
+                break
+            self.bits.append(x)
 
     def __eq__(self, other):
+        if len(self.bits) != len(other.bits):
+            return False
         return self.bits == other.bits
+
+    def __repr__(self):
+        b = ['1' if x else '0' for x in self.bits]
+        return ''.join(b)
 
     def __len__(self):
         return len(self.bits)
 
-    def __repr__(self):
-        return self.bits.bin
+    def __del__(self):
+        self.f.close()
+
+
+def test_compress_expand(ori_files, com_files, exp_files, algs):
+    for of, cf, ef in zip(ori_files, com_files, exp_files):
+        b_com = time.time()
+        algs.compress(of, cf)  # 压缩, origin -> compress
+        e_com = time.time()
+        algs.expand(cf, ef)  # 解压缩, compress -> origin
+        e_exp = time.time()
+        of_bits, cf_bits, ef_bits = FileBits(of), FileBits(cf), FileBits(ef)
+        print(of)
+        print('len', len(of_bits), '->', len(cf_bits), 'rate', len(cf_bits) / len(of_bits))
+        print('compress {}s, expand {}s'.format(e_com - b_com, e_exp - e_com))
+        assert of_bits == ef_bits  # origin bits == expand bits
+        print()
 
 
 def test_run_length():
+    print('-' * 30, 'RunLength', '-' * 30)
     ori_files = ['data/4runs.bin',
                  'data/abra.txt',
                  'data/q32x48.bin',
@@ -32,19 +64,38 @@ def test_run_length():
                  'temp_files/abra.txt',
                  'temp_files/q32x48.bin',
                  'temp_files/q64x96.bin']
-    for of, cf, ef in zip(ori_files, com_files, exp_files):
-        RunLength.compress(of, cf)  # 压缩, origin -> compress
-        RunLength.expand(cf, ef)  # 解压缩, compress -> origin
-        of_bits, cf_bits, ef_bits = FileBits(of), FileBits(cf), FileBits(ef)
-        print(of)
-        print('len', len(of_bits), len(cf_bits), 'rate', len(cf_bits) / len(of_bits))
-        assert of_bits == ef_bits  # origin bits == expand bits
-        print()
+    test_compress_expand(ori_files, com_files, exp_files, RunLength)
 
 
 def test_huffman():
-    pass
+    print('-' * 30, 'Huffman', '-' * 30)
+    ori_files = ['data/4runs.bin',
+                 'data/abra.txt',
+                 'data/q32x48.bin',
+                 'data/q64x96.bin',
+                 'data/tinytinyTale.txt',
+                 'data/tinyTale.txt',
+                 'data/medTale.txt',
+                 'data/tale.txt']
+    com_files = ['temp_files/4runs.bin.huffman',
+                 'temp_files/abra.txt.huffman',
+                 'temp_files/q32x48.bin.huffman',
+                 'temp_files/q64x96.bin.huffman',
+                 'temp_files/tinytinyTale.txt.huffman',
+                 'temp_files/tinyTale.txt.huffman',
+                 'temp_files/medTale.txt.huffman',
+                 'temp_files/tale.txt.huffman']
+    exp_files = ['temp_files/4runs.bin',
+                 'temp_files/abra.txt',
+                 'temp_files/q32x48.bin',
+                 'temp_files/q64x96.bin',
+                 'temp_files/tinytinyTale.txt',
+                 'temp_files/tinyTale.txt',
+                 'temp_files/medTale.txt',
+                 'temp_files/tale.txt']
+    test_compress_expand(ori_files, com_files, exp_files, Huffman)
 
 
 if __name__ == '__main__':
     test_run_length()
+    test_huffman()
